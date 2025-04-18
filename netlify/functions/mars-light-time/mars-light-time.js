4ff586a682c7630f8b5e1cea20574d24d94c754b
AspiringMartians <contact@aspiringmartians.com>
2025-04-18 11:08:08 -0400

    Update mars-light-time.js

--- netlify/functions/mars-light-time/mars-light-time.js
+++ netlify/functions/mars-light-time/mars-light-time.js
@@ -5,27 +5,18 @@ const APP_SECRET = "4e77ffa1a27bde49e7cc0e54a9cd50ed4839838e3d4dcc0286aeee53";
 
 exports.handler = async function () {
   try {
-    const authRes = await fetch("https://api.astronomyapi.com/api/v2/basic-auth", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ app_id: APP_ID, app_secret: APP_SECRET })
-    });
-
-const authData = await authRes.json();
-
-if (!authData.data || !authData.data.access_token) {
-  throw new Error(`Authentication failed: ${JSON.stringify(authData)}`);
-}
-
-const token = authData.data.access_token;
-
-
     const today = new Date().toISOString().split('T')[0];
-    const positionRes = await fetch(`https://api.astronomyapi.com/api/v2/bodies/positions/mars?latitude=0&longitude=0&elevation=0&from_date=${today}&to_date=${today}&time=00:00:00`, {
-      headers: { Authorization: `Bearer ${token}` }
+
+    const url = `https://api.astronomyapi.com/api/v2/bodies/positions/mars?latitude=0&longitude=0&elevation=0&from_date=${today}&to_date=${today}&time=00:00:00`;
+
+    const response = await fetch(url, {
+      headers: {
+        Authorization: 'Basic ' + Buffer.from(`${APP_ID}:${APP_SECRET}`).toString('base64')
+      }
     });
 
-    const positionData = await positionRes.json();
+    const positionData = await response.json();
+
     const distanceKm = positionData.data.table.rows[0].cells[0].distance.fromEarth.km;
 
     const lightSpeed = 299792.458;
